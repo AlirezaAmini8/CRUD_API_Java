87690fea5177b2270bb31478c07e4ec0d06bb18e
Alirezaamini8 <Alirezaamini.138115@gmail.com>
2023-08-20 11:34:21 +0330

    database connection logs added.

--- Test_CRUD_API/src/main/java/com/example/controller/DatabaseConnection.java
+++ Test_CRUD_API/src/main/java/com/example/controller/DatabaseConnection.java
@@ -1,15 +1,27 @@
 package com.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseConnection.class);
+
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String DB_USER = "postgres";
     private static final String DB_PASSWORD = "4271";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            logger.info("Database connection established successfully.");
+            return connection;
+        } catch (SQLException e) {
+            logger.error("Failed to establish a database connection: {}", e.getMessage());
+            throw e;
+        }
     }
 }
